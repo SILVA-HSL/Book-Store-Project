2fab47ab54a12c1dc0a9c1458b0f308b5e8d26d7
Shanuka Lakshan <121119338+SILVA-HSL@users.noreply.github.com>
2024-09-17 18:01:48 +0530

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 3000;
+
+export const mongoDBURL = 'mongodb+srv://shanukalakshan:hsls5071@book-store.ogpug.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store';
---
+++ backend/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
+import booksRoute from './routes/booksRoute.js';
+
+const app = express();
+
+app.use(express.json());
+
+//middleware to handle cors policy error
+//option1:allow all oringins with default of cors(*)
+app.use(cors());
+
+//option2:allow only specific origins
+app.use(cors({
+    origin: 'http://localhost:3000',
+    method: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}));
+
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(234).send('Hello World!');
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+.connect(mongoDBURL)
+.then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port: ${PORT}`);
+    });
+})
+.catch((error) => {
+    console.log('Error connecting to MongoDB: ', error.message);
+});
